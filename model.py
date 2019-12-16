18e2fb0ea8e26902db067cf41153f284699be999
Emil <ejoswin@gmail.com>
2019-12-16 02:37:00 -0500

    cnn 0.65 done

--- model.py
+++ model.py
@@ -69,7 +69,7 @@ class MyBasicAttentiveBiGRU(models.Model):
         return {'logits': logits}
 
 
-class MyAdvancedModel(models.Model):
+class MyAdvancedModel1(models.Model):
 
     def __init__(self, vocab_size: int, embed_dim : int, hidden_size: int = 128, training: bool = False):
         super(MyAdvancedModel, self).__init__()
@@ -79,7 +79,8 @@ class MyAdvancedModel(models.Model):
         self.num_classes = len(ID_TO_CLASS)
 
         self.decoder = layers.Dense(units=self.num_classes)
-        self.drop = layers.Dropout(0.3)
+        self.drop = layers.Dropout(0.5)
+        self.drop_emb = layers.Dropout(0.3)
         self.omegas = tf.Variable(tf.random.normal((hidden_size*2, 1)))
         self.embeddings = tf.Variable(tf.random.normal((vocab_size, embed_dim)))
 
@@ -91,18 +92,18 @@ class MyAdvancedModel(models.Model):
 
         layer2 = models.Sequential()
         forward_layer = layers.GRU(hidden_size, return_sequences=True, dropout=0.3) # TODO -emil - try other initializers
-        backward_layer = layers.GRU(hidden_size, return_sequences=True, go_backwards=True, dropout=0.3)
+        backward_layer = layers.GRU(hidden_size, return_sequences=True, go_backwards=True, dropout=0.5)
         layer2.add(layers.Bidirectional(forward_layer, backward_layer=backward_layer,
                          input_shape=(5, 2*hidden_size))) 
 
         layer3 = models.Sequential()
-        forward_layer = layers.GRU(hidden_size, return_sequences=True, dropout=0.5) # TODO -emil - try other initializers
+        forward_layer = layers.GRU(hidden_size, return_sequences=True, dropout=0.3) # TODO -emil - try other initializers
         backward_layer = layers.GRU(hidden_size, return_sequences=True, go_backwards=True, dropout=0.5)
         layer3.add(layers.Bidirectional(forward_layer, backward_layer=backward_layer,
                          input_shape=(5, 2*hidden_size))) 
 
         layer4 = models.Sequential()
-        forward_layer = layers.GRU(hidden_size, return_sequences=True, dropout=0.5) # TODO -emil - try other initializers
+        forward_layer = layers.GRU(hidden_size, return_sequences=True, dropout=0.3) # TODO -emil - try other initializers
         backward_layer = layers.GRU(hidden_size, return_sequences=True, go_backwards=True, dropout=0.5)
         layer4.add(layers.Bidirectional(forward_layer, backward_layer=backward_layer,
                          input_shape=(5, 2*hidden_size))) 
@@ -136,10 +137,9 @@ class MyAdvancedModel(models.Model):
         word_embed = tf.nn.embedding_lookup(self.embeddings, inputs)
         pos_embed = tf.nn.embedding_lookup(self.embeddings, pos_inputs)
 
-        ### TODO(Students) START
-        # ...
-        ### TODO(Students) END
-        # print('training', training)
+        if training:
+            word_embed = self.drop_emb(word_embed)
+        
         tokens_mask = tf.cast(inputs!=0, tf.float32)
 
         x = tf.concat([word_embed, pos_embed], axis=2)
@@ -164,3 +164,63 @@ class MyAdvancedModel(models.Model):
             logits = self.decoder(logits)
 
         return {'logits': logits}
+
+
+class MyAdvancedModel(models.Model):
+
+    def __init__(self, vocab_size: int, embed_dim : int, hidden_size: int = 128, training: bool = False):
+        super(MyAdvancedModel, self).__init__()
+
+        self.num_classes = len(ID_TO_CLASS)
+        self.decoder = layers.Dense(units=self.num_classes)
+        self.embeddings = tf.Variable(tf.random.normal((vocab_size, embed_dim)))
+
+        self.drop = layers.Dropout(0.3)
+        embed_dim = 5
+        seq_len = 5
+        n_filters = 100 
+        
+        filter_size = 2 #number of words seen during one conv operation
+        self.conv1 = layers.Conv1D(n_filters, filter_size)
+        
+        filter_size = 3 #number of words seen during one conv operation
+        self.conv2 = layers.Conv1D(n_filters, filter_size)
+        
+        filter_size = 4 #number of words seen during one conv operation
+        self.conv3 = layers.Conv1D(n_filters, filter_size)
+        
+        self.dropout = layers.Dropout(0.5)
+        self.linear = layers.Dense(units=self.num_classes)
+
+
+    def attn(self, outputs):
+        pass
+
+    def call(self, inputs, pos_inputs, training):
+        word_embed = tf.nn.embedding_lookup(self.embeddings, inputs)
+        pos_embed = tf.nn.embedding_lookup(self.embeddings, pos_inputs)
+
+        tokens_mask = tf.cast(inputs!=0, tf.float32)
+
+        x = tf.concat([word_embed, pos_embed], axis=2)
+
+        x = self.drop(x)
+        # print(inputs.shape)
+        out1 = tf.nn.tanh( self.conv1(x) ) # bsz x (filter_size - seq_len + 1) x n_filters
+        out2 = tf.nn.tanh( self.conv2(x) )
+        out3 = tf.nn.tanh( self.conv3(x) )
+        
+        # bsz x 1 x n_filters - before squeeze
+        # bsz x n_filters - after squeeze
+        pool1 = tf.squeeze( layers.MaxPool1D(out1.shape[1])(out1) ) 
+        pool2 = tf.squeeze( layers.MaxPool1D(out2.shape[1])(out2) )
+        pool3 = tf.squeeze( layers.MaxPool1D(out3.shape[1])(out3) ) 
+        
+        # bsz * n_filters * (number of different conv2d layers)
+        x = tf.concat([pool1, pool2, pool3], axis=1)
+        
+        x = self.dropout(x)
+        
+        x = self.linear(x)
+
+        return {'logits': x}
